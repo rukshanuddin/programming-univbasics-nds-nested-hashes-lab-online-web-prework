31f36af002aad3a54891390ae95e1692c78c5dae
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 20:14:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
+  aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  aoh[row][key] = new_value# Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
